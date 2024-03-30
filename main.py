564e62cfc422fc8d371c76e224647f6f11d77715
PavelAnkl <kluev-pa@yandex.ru>
2024-03-30 13:53:12 +0300

    start public job

--- main.py
+++ main.py
@@ -1,48 +1,5 @@
-# Родительский класс
-class Bird():
-    def __init__(self, name, voice, color):
-        self.name = name
-        self.voice = voice
-        self.color = color
-    def fly(self):
-        print(f'{self.name} летает')
-    def eat(self):
-        print(f'{self.name} кушает')
-    def sing(self):
-        # print(f'{self.name} поет - {self.voice}')
-        print(f'{self.name} поет - чирик')
-    def info(self):
-        print(f'{self.name} - имя')
-        print(f'{self.voice} - голос')
-        print(f'{self.color} - окрас птицы')
-
-# Дочерний класс
-class Pigeon(Bird):
-    def __init__(self, name, voice, color, favorite_food):
-        super().__init__(name, voice, color)
-        self.favorite_food = favorite_food
-
-    def sing(self):
-        print(f'{self.name} поет - курлык')
-
-    def walk(self):
-        print(f'{self.name} гуляет')
-
-bird1 = Pigeon('Гоша', 'курлык', 'серый', 'хлебные крошки')
-
-bird2 = Bird('Маша', 'чирик', 'коричневый')
-
-bird1.sing()
-bird1.info()
-bird1.walk()
-
-
-
-
-
-
-
-
-
-
-
+class Test():
+    def __init__(self):
+        self.public = 'публичный атрибут'
+        self._protected = 'защищенный атрибут'
+        self.__private = 'приватный атрибут'
